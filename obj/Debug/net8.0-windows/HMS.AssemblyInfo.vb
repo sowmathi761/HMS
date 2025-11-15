5bd37b37ccf7f0a952442d4cd64c2a9cc89b00fb
sowmathi761 <sowmathi37@gmail.com>
2025-11-15 19:45:10 +0530

    Remove build artifacts, add .gitignore

--- obj/Debug/net8.0-windows/HMS.AssemblyInfo.vb
+++ obj/Debug/net8.0-windows/HMS.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("HMS"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7390ba74189078c3ff62de7b9be7de7617145119"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("HMS"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("HMS"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
